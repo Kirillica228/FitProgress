18073287b24a044b60b34025febc20b2439e2f8d
Kirillica228 <kirillicar8@gmail.com>
2026-04-23 17:01:20 +0400

    Удаление бота

--- bot/handlers/__init__.py
+++
@@ -1,7 +0,0 @@
-from aiogram import Dispatcher
-
-from bot.handlers.workout import router as workout_router
-
-
-def setup_routers(dispatcher: Dispatcher) -> None:
-    dispatcher.include_router(workout_router)
--- bot/states.py
+++
@@ -1,9 +0,0 @@
-from aiogram.fsm.state import State, StatesGroup
-
-
-class WorkoutStates(StatesGroup):
-    choosing_type = State()
-    choosing_difficulty = State()
-    ready_to_start = State()
-    in_progress = State()
-    waiting_result = State()
