e7032333c33ff3c79a8df683dadd2863349e4b67
Kirillica228 <kirillicar8@gmail.com>
2026-06-17 00:59:42 +0400

    Добавление статики для админки

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -151,7 +151,8 @@ USE_TZ = True
 
 
 # Static files
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
